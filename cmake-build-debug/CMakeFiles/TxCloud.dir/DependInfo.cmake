95a37b6e0b7c6aa91207ddc5bfd04ccdc7467178
Anton <larin.an10n@gmail.com>
2019-09-04 21:51:23 +0300

    idk whatsthat

---
+++ cmake-build-debug/CMakeFiles/TxCloud.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/anton/Git/TxCloud/KR_1_2/TxPaint/TxPaint/Control.c" "/home/anton/Git/TxCloud/cmake-build-debug/CMakeFiles/TxCloud.dir/KR_1_2/TxPaint/TxPaint/Control.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../KR_1_2/TxPaint/TxPaint"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/anton/Git/TxCloud/KR_1_2/TxPaint/TxPaint/ArrayStack.cpp" "/home/anton/Git/TxCloud/cmake-build-debug/CMakeFiles/TxCloud.dir/KR_1_2/TxPaint/TxPaint/ArrayStack.cpp.o"
+  "/home/anton/Git/TxCloud/KR_1_2/TxPaint/TxPaint/Control.cpp" "/home/anton/Git/TxCloud/cmake-build-debug/CMakeFiles/TxCloud.dir/KR_1_2/TxPaint/TxPaint/Control.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../KR_1_2/TxPaint/TxPaint"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/TxCloud.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TxCloud.dir/KR_1_2/TxPaint/TxPaint/ArrayStack.cpp.o"
+  "CMakeFiles/TxCloud.dir/KR_1_2/TxPaint/TxPaint/Control.c.o"
+  "CMakeFiles/TxCloud.dir/KR_1_2/TxPaint/TxPaint/Control.cpp.o"
+  "TxCloud.pdb"
+  "TxCloud"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/TxCloud.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
