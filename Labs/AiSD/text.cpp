0dd28bc4dc3f8ab9a68fe963fa8f990dd1db440c
Anton <larin.an10n@gmail.com>
2019-11-05 15:04:18 +0300

    Cleanup (messy one)

---
+++ Labs/AiSD/text.cpp
@@ -0,0 +1,20 @@
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	char* str1 = (char*)malloc(100);	
+	char* str2 = (char*)malloc(100);
+	char* str3 = (char*)malloc(100);
+	
+	cin>>str1;
+	cin>>str2;
+	cin>>str3;
+	
+	
+	cout<<str1<<endl;
+	cout<<str2<<endl;
+	cout<<str3<<endl;
+	
+}
