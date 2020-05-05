000d0521917916ee6ea2eaafa52ab7088d5b6692
Anton <larin.an10n@gmail.com>
2020-05-06 02:58:24 +0300

    OOP new beginning

---
+++ Labs/s2/OOP/OOProject/cmake-build-debug/CMakeFiles/OOProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OOProject.dir/main.cpp.o"
+  "OOProject.pdb"
+  "OOProject"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OOProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
