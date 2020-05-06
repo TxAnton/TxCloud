29e4ad57f9998bf6c4069b007d433a1aac2e602a
Anton <larin.an10n@gmail.com>
2020-05-06 03:03:33 +0300

    Alg 5 done

---
+++ Labs/s2/Alg/5_Aho/cmake-build-debug/CMakeFiles/5_Aho.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/5_Aho.dir/lab5.cpp.o"
+  "5_Aho.pdb"
+  "5_Aho"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/5_Aho.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
